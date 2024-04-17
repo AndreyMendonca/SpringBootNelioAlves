b9510c5bb194e55e4c8b18665a34cc78d0103252
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-04-17 19:34:36 -0300

    Classe Product e metodos implementados

--- SB-SistemaDeVenda/src/main/java/com/example/demo/entities/Category.java
+++ SB-SistemaDeVenda/src/main/java/com/example/demo/entities/Category.java
@@ -1,20 +1,25 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @Table(name="tb_category")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
@@ -25,4 +30,7 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	
+	@OneToMany
+	private Set<Product> products = new HashSet<>();	
 }
