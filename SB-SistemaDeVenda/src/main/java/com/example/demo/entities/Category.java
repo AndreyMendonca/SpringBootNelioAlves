9ad8a794f5574ed2914bcff22f7d6aa57da3cc40
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-04-18 13:42:45 -0300

    Classe Payment e metodos implementados

--- SB-SistemaDeVenda/src/main/java/com/example/demo/entities/Category.java
+++ SB-SistemaDeVenda/src/main/java/com/example/demo/entities/Category.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,6 +33,7 @@ public class Category implements Serializable{
 	private Integer id;
 	private String name;
 	
-	@OneToMany
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();	
 }
--- SB-SistemaDeVenda/src/main/java/com/example/demo/entities/Order.java
+++ SB-SistemaDeVenda/src/main/java/com/example/demo/entities/Order.java
@@ -2,15 +2,20 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.example.demo.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -36,5 +41,11 @@ public class Order implements Serializable {
 	private User client;
 	
 	private OrderStatus orderStatus;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy="order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 }
